cf2fee48742b71b723995c318d0e224a0c2b15b1
ronit-nebulaiq <ronit.aryan@nebulaiq.ai>
2024-10-02 12:46:35 +0530

    getting started

---
+++ src/components/Physarum/Physarum.tsx
@@ -0,0 +1,18 @@
+'use client'
+
+import { useState } from "react";
+
+interface PhysarumProps {
+  width: number;
+  height: number;
+} 
+
+export const Physarum: React.FC<PhysarumProps> = ({width, height}) => {
+  
+  
+
+  
+  return (
+    <div> Physarum </div> 
+  )
+}
